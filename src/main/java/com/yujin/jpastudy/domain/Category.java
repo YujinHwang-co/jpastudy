56fd5ede4d46404524ad6f9f76d5ceb66b7f0262
YujinHwang-co <jaevelly214@gmail.com>
2023-01-27 00:36:42 +0900

    도메인 분석설계_엔티티 설계시 주의점

--- src/main/java/com/yujin/jpastudy/domain/Category.java
+++ src/main/java/com/yujin/jpastudy/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -25,10 +27,16 @@ public class Category {
     ) // RDBMS 는 1:N or N:1로 풀어내는 중간테이블(category_item 테이블) 필요
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    /* 연관관계 메서드 */
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
